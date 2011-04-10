095ccbdb3ab12439120f30ecea282758b3741aab
Mark Haskamp <mrhask@gmail.com>
2011-04-10 16:40:58 -0400

    refactoring

--- lib/FizzBuzz.rb
+++ lib/FizzBuzz.rb
@@ -1,34 +1,33 @@
 
 class Mod5
-  def self.fizzbuzz n
-    return "buzz" if n % 5 == 0
-  end
-
   def self.is_buzz? n
     return n % 5 == 0
+  end
+
+  def self.fizzbuzz_string
+    "buzz"
   end
 end
 
 class Mod3
-  def self.fizzbuzz n
+  def fizzbuzz n
     if n % 3 == 0
       return "fizzbuzz" if Mod5.is_buzz? n
       return "fizz"
     end
 
-    return 'buzz'
+    return Mod5.fizzbuzz_string
   end
 end
 
 class FizzBuzz
   def fizzbuzz n
-    return_val = ""
 
     return 0 if n == 0
     return n if n < 3
 
-    return_val = Mod5.fizzbuzz n
-    return_val = Mod3.fizzbuzz n
+    fb3 = Mod3.new
+    fb3.fizzbuzz n
 
   end
 end
