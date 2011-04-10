e89f7ec523a58642515a49d6d4866075c32ed149
Mark Haskamp <mrhask@gmail.com>
2011-04-09 22:59:49 -0400

    moved hash up to a class variable

--- lib/FizzBuzz.rb
+++ lib/FizzBuzz.rb
@@ -1,4 +1,6 @@
 class FizzBuzz
+  @fizzbuzz_map = {1=>'fizz', 2=>'buzz',3=>'fizzbuzz'}
+
   def self.for n
     return 0 if n == 0
 
@@ -6,6 +8,7 @@ class FizzBuzz
     fizzbuzz_count += 1 if n%3 == 0
     fizzbuzz_count += 2 if n%5 == 0
 
-    return {0=>n, 1=>'fizz', 2=>'buzz',3=>'fizzbuzz'}[fizzbuzz_count]
+    return n if fizzbuzz_count == 0
+    return @fizzbuzz_map[fizzbuzz_count]
   end
 end
