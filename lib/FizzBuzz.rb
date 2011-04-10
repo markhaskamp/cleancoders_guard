51160ab1877c06b1c5c661249f413956f75fe121
Mark Haskamp <mrhask@gmail.com>
2011-04-09 23:02:52 -0400

    lotsa ifs

--- lib/FizzBuzz.rb
+++ lib/FizzBuzz.rb
@@ -1,11 +1,9 @@
 class FizzBuzz
   def self.for n
     return 0 if n == 0
-
-    fizzbuzz_count = 0
-    fizzbuzz_count += 1 if n%3 == 0
-    fizzbuzz_count += 2 if n%5 == 0
-
-    return {0=>n, 1=>'fizz', 2=>'buzz',3=>'fizzbuzz'}[fizzbuzz_count]
+    return 'fizzbuzz' if ((n%3 == 0) && (n%5 == 0))
+    return 'fizz' if (n%3 == 0)
+    return 'buzz' if (n%5 == 0)
+    return n
   end
 end
