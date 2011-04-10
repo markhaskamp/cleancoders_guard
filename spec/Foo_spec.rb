4827a66678b299787938aead9e5e8da2bdbcc23c
Mark Haskamp <mrhask@gmail.com>
2011-04-09 22:44:31 -0400

    correced test in Foo_spec.rb

--- spec/Foo_spec.rb
+++ spec/Foo_spec.rb
@@ -2,6 +2,6 @@ require File.dirname(__FILE__) + '/../lib/Foo.rb'
 
 describe Foo do
   it 'life, the universe, everything = 42' do
-    Foo.new().life_universe_everything? == 42
+    Foo.new().life_universe_everything?.should == 42
   end
 end
